563edd61d3f91a1b0184523017a805402583835a
imji0319 <tfj0531@gmail.com>
2021-01-20 20:38:23 +0900

    update comp

--- 증권데이터분석/Investar/DBUpdater.py
+++ 증권데이터분석/Investar/DBUpdater.py
@@ -10,6 +10,7 @@ Created on Wed Jan 20 18:18:40 2021
 
 import pymysql
 import pandas as pd 
+from datetime import datetime
 
 class DBUpdater:
     
@@ -72,6 +73,43 @@ class DBUpdater:
         
     def update_comp_info(self):
         """종목코드를 company_info 테이블에 업데이트한 후 딕셔너리에 저장 """
+        
+        sql = "SELECT * FROM company_info"
+        
+        df = pd.read_sql(sql, self.conn)
+        
+        for idx in range(len(df)):
+            self.codes[df['code'].values[idx]] = df['company'].values[idx]
+            
+        with self.conn.cursor() as curs :
+            sql = "SELECT max(last_update) FROM company_info"
+            curs.execute(sql)
+            rs = curs.fetchone() # 
+            today = datetime.today().strftime('%Y-%m-%d')
+            
+            
+            if rs[0] == None or rs[0].strftime('%Y-%m-%d') < today :
+                krx = self.read_krx_code()
+                
+                for idx in range(len(krx)):
+                    code = krx.code.values[idx]
+                    company = krx.company.values[idx]
+                    
+                    sql = f"REPLACE INTO company_info (code, company, last"\
+                        f"_update) VALUES ('{code}', '{company}', '{today}') "
+                    
+                    curs.execute(sql)
+                    
+                    self.codes[code] =company
+                    tmnow = datetime.now().strftime('%Y-%m-%d %H:%H')
+                    print(f"[{tmnow}] {idx:04d} REPLACE INTO company_info "\
+                          f"VALUES ({code}, {company},{today})")
+                        
+                self.conn.commit()
+                print('')
+                      
+            
+            
         
     def read_naver(self, code, company, pages_to_fetch):
         """ 네이버 금융애서 주식 시세를 읽어서 데이터프레임으로 반환 """
