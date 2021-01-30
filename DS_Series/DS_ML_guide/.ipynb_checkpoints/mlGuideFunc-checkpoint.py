77cd51f166e3f6b0c42f147730db5012ce0bc287
imji0319 <tfj0531@gmail.com>
2021-01-30 15:22:17 +0900

    visual

--- DS_Series/DS_ML_guide/.ipynb_checkpoints/mlGuideFunc-checkpoint.py
+++ DS_Series/DS_ML_guide/.ipynb_checkpoints/mlGuideFunc-checkpoint.py
@@ -6,8 +6,10 @@ Created on Thu Dec  3 23:31:03 2020
 @author: jihye
 """
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.preprocessing import Binarizer
+
 
 # 오차 행렬, 정확도, 정밀도, 재현율 
 def get_clf_eval(y_test, pred):
@@ -16,25 +18,160 @@ def get_clf_eval(y_test, pred):
     accuracy = accuracy_score(y_test, pred)
     precision = precision_score(y_test, pred)
     recall = recall_score(y_test, pred)
+    f1 = f1_score(y_test, pred)
+    auc = roc_auc_score(y_test, pred)
     
-    return confusion, accuracy, precision, recall 
+    print("오차행렬")
+    print(confusion)
+    print("정확도 : {:.4f} , 정밀도 : {:.4f}, 재현율 : {:.4f}, F1 : {:.4f}, auc : {:.4f}"
+          .format(accuracy, precision, recall, f1, auc) )
 
 # 임계값 변경 
-
 def get_eval_by_threshold(y_test, pred_proba_c1, threshold):
-    
-    capr = []
+
     # threshold list 객체 내의 값을 차례로 iteration 하면서 Evaluation 수행
     for custom_th in threshold: 
         binarizer = Binarizer(threshold = custom_th).fit(pred_proba_c1)
         custom_pred = binarizer.transform(pred_proba_c1)
         
-        for i in  get_clf_eval(y_test, custom_pred):
-            capr.append(i)
+        
+        print('임계값 :', custom_th)
+        get_clf_eval(y_test, custom_pred)
+        
+# 정밀도와 재현율의 임계값에 따른 값의 변화 그래프 
+import matplotlib.pyplot as plt 
+import numpy as np 
+from sklearn.metrics import precision_recall_curve
+
+def precision_recall_curve_plot(y_test, pred_proba_c1):
+    
+    precision, recalls , thresholds = precision_recall_curve(y_test, pred_proba_c1)
+    
+    plt.figure(figsize = (8,6))
+    threshold_boundary = thresholds.shape[0]
+    plt.plot(thresholds, precision[0:threshold_boundary], linestyle = '--', label = 'precision')
+    plt.plot(thresholds, recalls[0:threshold_boundary], label = 'recall')
+    
+    start, end = plt.xlim()
+    
+    plt.xticks(np.round(np.arange(start, end, 0.1), 2))
+    
+    plt.xlabel('Threshold value'); plt.ylabel('Precision and Recall value')
+    plt.legend(); plt.grid()
+    plt.show()
+    
+
+from sklearn.metrics import roc_curve
+
+def roc_curve_plot(y_test, pred_proba_c1):
+    
+    fprs, tprs, thresholds = roc_curve(y_test, pred_proba_c1)
+    
+    plt.plot(fprs, tprs, label = 'ROC')
+    plt.plot([0,1], [0,1], 'k--', label ='Random')
+    
+    start, end = plt.xlim()
+    plt.xticks(np.round(np.arange(start, end, 0.1),2))
+    plt.xlim(0,1) ; plt.ylim(0,1)
+    plt.xlabel('FPR (1- Sensitivity'); plt.ylabel('TPR ( Recall )')
+    plt.legend()
+    plt.show()
+    
+### 여러개의 클러스터링 갯수를 List로 입력 받아 각각의 클러스터링 결과를 시각화 
+def visualize_kmeans_plot_multi(cluster_lists, X_features):
+    
+    from sklearn.cluster import KMeans
+    from sklearn.decomposition import PCA
+    import pandas as pd
+    import numpy as np
+    
+    # plt.subplots()으로 리스트에 기재된 클러스터링 만큼의 sub figures를 가지는 axs 생성 
+    n_cols = len(cluster_lists)
+    fig, axs = plt.subplots(figsize=(4*n_cols, 4), nrows=1, ncols=n_cols)
+    
+    # 입력 데이터의 FEATURE가 여러개일 경우 2차원 데이터 시각화가 어려우므로 PCA 변환하여 2차원 시각화
+    pca = PCA(n_components=2)
+    pca_transformed = pca.fit_transform(X_features)
+    dataframe = pd.DataFrame(pca_transformed, columns=['PCA1','PCA2'])
+    
+     # 리스트에 기재된 클러스터링 갯수들을 차례로 iteration 수행하면서 KMeans 클러스터링 수행하고 시각화
+    for ind, n_cluster in enumerate(cluster_lists):
+        
+        # KMeans 클러스터링으로 클러스터링 결과를 dataframe에 저장. 
+        clusterer = KMeans(n_clusters = n_cluster, max_iter=500, random_state=0)
+        cluster_labels = clusterer.fit_predict(pca_transformed)
+        dataframe['cluster']=cluster_labels
+        
+        unique_labels = np.unique(clusterer.labels_)
+        markers=['o', 's', '^', 'x', '*']
+       
+        # 클러스터링 결과값 별로 scatter plot 으로 시각화
+        for label in unique_labels:
+            label_df = dataframe[dataframe['cluster']==label]
+            if label == -1:
+                cluster_legend = 'Noise'
+            else :
+                cluster_legend = 'Cluster '+str(label)           
+            axs[ind].scatter(x=label_df['PCA1'], y=label_df['PCA2'], s=70,\
+                        edgecolor='k', marker=markers[label], label=cluster_legend)
+
+        axs[ind].set_title('Number of Cluster : '+ str(n_cluster))    
+        axs[ind].legend(loc='upper right')
+    
+    plt.show()
+    
+### 여러개의 클러스터링 갯수를 List로 입력 받아 각각의 실루엣 계수를 면적으로 시각화한 함수 작성
+def visualize_silhouette(cluster_lists, X_features): 
+    
+    from sklearn.datasets import make_blobs
+    from sklearn.cluster import KMeans
+    from sklearn.metrics import silhouette_samples, silhouette_score
+
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    import math
+    
+    # 입력값으로 클러스터링 갯수들을 리스트로 받아서, 각 갯수별로 클러스터링을 적용하고 실루엣 개수를 구함
+    n_cols = len(cluster_lists)
+    
+    # plt.subplots()으로 리스트에 기재된 클러스터링 수만큼의 sub figures를 가지는 axs 생성 
+    fig, axs = plt.subplots(figsize=(4*n_cols, 4), nrows=1, ncols=n_cols)
+    
+    # 리스트에 기재된 클러스터링 갯수들을 차례로 iteration 수행하면서 실루엣 개수 시각화
+    for ind, n_cluster in enumerate(cluster_lists):
+        
+        # KMeans 클러스터링 수행하고, 실루엣 스코어와 개별 데이터의 실루엣 값 계산. 
+        clusterer = KMeans(n_clusters = n_cluster, max_iter=500, random_state=0)
+        cluster_labels = clusterer.fit_predict(X_features)
+        
+        sil_avg = silhouette_score(X_features, cluster_labels)
+        sil_values = silhouette_samples(X_features, cluster_labels)
+        
+        y_lower = 10
+        axs[ind].set_title('Number of Cluster : '+ str(n_cluster)+'\n' \
+                          'Silhouette Score :' + str(round(sil_avg,3)) )
+        axs[ind].set_xlabel("The silhouette coefficient values")
+        axs[ind].set_ylabel("Cluster label")
+        axs[ind].set_xlim([-0.1, 1])
+        axs[ind].set_ylim([0, len(X_features) + (n_cluster + 1) * 10])
+        axs[ind].set_yticks([])  # Clear the yaxis labels / ticks
+        axs[ind].set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+        
+        # 클러스터링 갯수별로 fill_betweenx( )형태의 막대 그래프 표현. 
+        for i in range(n_cluster):
+            ith_cluster_sil_values = sil_values[cluster_labels==i]
+            ith_cluster_sil_values.sort()
+            
+            size_cluster_i = ith_cluster_sil_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+            
+            color = cm.nipy_spectral(float(i) / n_cluster)
+            axs[ind].fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_sil_values, \
+                                facecolor=color, edgecolor=color, alpha=0.7)
+            axs[ind].text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+            y_lower = y_upper + 10
+            
+        axs[ind].axvline(x=sil_avg, color="red", linestyle="--")
 
         
-    return capr
 
-
-    
-    
