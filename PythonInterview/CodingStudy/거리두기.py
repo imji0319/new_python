a805c989c0df62b38363d66a57521e8a98dc3f0e
imji0319 <tfj0531@gmail.com>
2021-07-17 13:05:47 +0900

    change

--- PythonInterview/CodingStudy/거리두기.py
+++ PythonInterview/CodingStudy/거리두기.py
@@ -23,6 +23,7 @@ def solution(places):
                     if i + 1 < 5:
                         if place[i + 1][j] == 'P':
                             answer[n] = 0
+
                     if j + 1 < 5:
                         if place[i][j + 1] == 'P':
                             answer[n] = 0
@@ -38,17 +39,7 @@ def solution(places):
                             if (place[i - 1][j] == 'O' or place[i][j + 1] == 'O'):
                                 answer[n] = 0
 
-                    # 왼쪽 아래 대각선
-                    if i + 1 < 5 and j - 1 > -1:
-                        if place[i + 1][j - 1] == 'P':
-                            if (place[i][j - 1] == 'O') or (place[i + 1][j] == 'O'):
-                                answer[n] = 0
 
-                    # 왼쪽 위 대각선
-                    if i - 1 > -1 and j - 1 > -1:
-                        if place[i - 1][j - 1] == 'P':
-                            if place[i][j - 1] == 'O' or place[i - 1][j] == 'O':
-                                answer[n] = 0
 
                     # 가로 세로 두칸씩
                     if i + 2 < 5:
@@ -60,3 +51,11 @@ def solution(places):
                             answer[n] = 0
 
     return answer
+
+
+
+lit = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+lit2 =[["OOPOO", "OPOOO", "OOOOO", "OOOOO", "OOOOO"], ["OOPOO", "OPOOO", "OOOOO", "OOOOO", "OOOOO"], ["OOPOO", "OPOOO", "OOOOO", "OOOOO", "OOOOO"], ["OOPOO", "OPOOO", "OOOOO", "OOOOO", "OOOOO"], ["OOPOO", "OPOOO", "OOOOO", "OOOOO", "OOOOO"]]
+
+print(solution(lit))
+print(solution(lit2))
