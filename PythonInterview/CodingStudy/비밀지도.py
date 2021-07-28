1a7a6f39c05bde0b6095fc205abbe41e5a71f348
imji0319 <tfj0531@gmail.com>
2021-07-28 14:16:22 +0900

    change

--- PythonInterview/CodingStudy/비밀지도.py
+++ PythonInterview/CodingStudy/비밀지도.py
@@ -1,17 +1,11 @@
-def rembin(s):
-    return s[2:]
-
-
 def solution(n, arr1, arr2):
     answer = []
 
-    arr1 = list(map(bin, arr1))
-    arr2 = list(map(bin, arr2))
-    arr1 = list(map(rembin, arr1))
-    arr2 = list(map(rembin, arr2))
-
     # 자릿수 채우기
     def corbin(s):
+        s = bin(s)
+        s = s[2:]
+
         if len(s) < n:
             ss = n - len(s)
             s = '0' * ss + s
