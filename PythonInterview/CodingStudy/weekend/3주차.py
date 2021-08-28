10a19e623ccac75556e0ed9ff1a79579163de38c
imji0319 <tfj0531@gmail.com>
2021-08-28 12:55:08 +0900

    회전가능

--- PythonInterview/CodingStudy/weekend/3주차.py
+++ PythonInterview/CodingStudy/weekend/3주차.py
@@ -50,46 +50,6 @@ def solution(game_board, table):
     ta = bb(table, 1)
 
 
-    #조각이 들어오면 0,0을 기준으로 패턴의 위치를 변경하고, 회전했을 때의 가능한 모든 조각 패턴 리턴
-    def rotate_shape_list(shapes):
-        list = []
-
-        # tuple -> array
-        sha = np.array(shapes)
-
-        # 패턴의 전체 크기 확인
-        x = max(sha[:,0]) - min(sha[:,0]) +1
-        y = max(sha[:,1]) - min(sha[:,1]) +1
-
-        ze = np.zeros((x,y))
-
-        # index값 변경 -> 원점 기준
-        if min(sha[:,0]) > 0:
-            sha[:,0] = sha[:,0] -min(sha[:,0])
-
-        if min(sha[:,1]) > 0:
-            sha[:,1] = sha[:,1] -min(sha[:,1])
-
-
-        for j in sha:
-            r, c = j
-            ze[r,c] = 1
-
-        list.append(ze)
-        list.append(np.rot90(ze))
-        list.append(np.rot90(ze,2))
-        list.append(np.rot90(ze,3))
-
-
-
-        return list
-
-
-    for i in ta:
-        sh_list = rotate_shape_list((i))
-        for j in sh_list:
-            print(j)
-
 
 
 
--- PythonInterview/CodingStudy/weekend/회전모양모두비교.py
+++ PythonInterview/CodingStudy/weekend/회전모양모두비교.py
@@ -3,3 +3,93 @@
 '''
 import numpy as np
 
+
+
+
+
+# 조각 비교
+def sP(board, sp):
+    #print('NEW')
+    #print(sp, len(sp), len(sp[0]), len(sp[1]))
+    # board : array, sp: array
+
+    one = True
+    for i in range(len(board)-len(sp)+1):
+        for j in range(len(board[0])-len(sp[0])+1):
+            ss = board[i:i+len(sp), j:j+len(sp[0])]
+            #print(i,j,'------')
+            #print(ss)
+
+            test = True
+            if np.array_equal(ss, sp):
+                #print(sp)
+                if i + len(sp) < len(board) and j > 0 :
+                    # 왼쪽
+                    if board[i:i+len(sp), j-1].any() != 0 :
+                        test = False
+                if j + len(sp[0]) <= len(board[0]) and i + len(sp) < len(board):
+                    # 아래
+                    if board[i+len(sp), j:j+len(sp[0])].any() != 0:
+                        test = False
+                if j + len(sp[0]) < len(board[0]) and i + len(sp) < len(board):
+                    # 오른쪽
+                    if board[i:i+len(sp),j+len(sp[0])].any() != 0:
+                        test = False
+
+                if j + len(sp[0]) < len(board[0]) and i > 0:
+                    # 위
+                    if board[i - 1, j:j + len(sp[0])].any() != 0 :
+                        test = False
+
+                if test:
+                    board[i:i+len(sp), j:j+len(sp[0])] = 0
+                    one = False
+                    break
+
+        if one == False:
+            break
+    return board
+
+
+# 회전 조각 list 반환
+def rotation(sp):
+    ro_list = []
+    ro_list.append(sp.tolist())
+    ro_list.append(np.rot90(sp).tolist())
+    ro_list.append(np.rot90(sp,2).tolist())
+    ro_list.append(np.rot90(sp,3).tolist())
+
+    return ro_list
+
+
+def rota_list(board, spss):
+
+    speces = [np.array(x) for x in spss]
+    #print(speces)
+    board = np.array(board)
+
+    speces = sorted(speces, key = lambda x:x.shape, reverse = True)
+
+    for i in speces:
+        ro_list = rotation(i)
+        #print(ro_list)
+        for j in ro_list:
+            board = sP(board, j)
+
+    return board
+
+
+emptyboard = [[0,0,1,0,0],
+              [0,1,1,1,1],
+              [0,0,1,0,0],
+              [0,0,0,0,0],
+              [0,1,1,0,0]]
+spece = [[[0, 1, 0],
+         [0, 1, 0],
+         [1, 1, 1]],
+         [[1],
+          [1]]]
+
+
+
+print(rota_list(emptyboard,spece))
