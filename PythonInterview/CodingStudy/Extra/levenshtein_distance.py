dcfe95e687ea626a91d5c4ac68f0c8efb900bd22
imji0319 <tfj0531@gmail.com>
2022-02-16 13:37:34 +0900

    change

--- PythonInterview/CodingStudy/Extra/levenshtein_distance.py
+++ PythonInterview/CodingStudy/Extra/levenshtein_distance.py
@@ -17,7 +17,7 @@ def levenshtein(s1, s2, debug = False ):
         for j, c2 in enumerate(s2):
             insertions = previous_row[j+1] + 1
             deletions = current_row[j] + 1
-            substitutions = previous_row[j] + (c1 != C2)
+            substitutions = previous_row[j] + (c1 != c2)
 
             current_row.append(min(insertions,
                                    deletions,
